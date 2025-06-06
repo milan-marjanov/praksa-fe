2e0bfb12792c677dce94919614a09986f6693f36
Bane <branislav.mirosavljev01@gmail.com>
2025-06-06 14:44:58 +0200

    fix: apiClient url fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { fetchHello } from './api/helloService'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -18,8 +18,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => fetchHello()}>
+          Button
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
