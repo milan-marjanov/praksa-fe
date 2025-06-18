efb5b9f29c3013d5865ba78e5f2f4beed071a008
TamaraRanisovic <ranisovic123@gmail.com>
2025-06-18 12:58:38 +0200

    Update AxiosClient.ts

--- src/axios/AxiosClient.ts
+++ src/axios/AxiosClient.ts
@@ -22,7 +22,7 @@ api.interceptors.request.use(
 api.interceptors.response.use(
   (res) => res,
   (error) => {
-    const { response } = error;
+    const { response, config } = error;
     if (!response) {
       console.error('Network error:', error);
       toast.error('Network error - please check your connection.');
@@ -30,6 +30,10 @@ api.interceptors.response.use(
     }
 
     const { status, data } = response;
+
+    if (status === 403 && config.url?.endsWith('/user/profile')) {
+      return Promise.reject(error);
+    }
     toast.error(data.message || `Error: ${status}`);
 
     return Promise.reject(error);
